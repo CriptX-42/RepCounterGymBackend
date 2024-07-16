d691209b60b4b66ee898e5aacbef5e64998948f2
Ricardo S. V. de Carvalho <ricardo-svc@live.com>
2024-07-16 20:14:50 -0300

    first commit

---
+++ src/test/java/com/RepCounterGym/RepCounterGym/RepCounterGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RepCounterGym.RepCounterGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepCounterGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
